6bdca710bd4f46e30c01bc9f854354054da42099
Michael Abraham Wekesa <wekesa360@outlook.com>
2022-01-07 21:40:13 +0300

    Update: Correct IdentityFile path

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -4,7 +4,7 @@
 include stdlib
 file_line { 'Declare identity file':
   path => '/etc/ssh/ssh_config',
-  line => '  IdentityFile ~/.ssh/holberton',
+  line => '  IdentityFile ~/.ssh/school',
   replace => true
 }
 
