9ce35953f66cdfa543bdaf78482ca2dd68365d6f
Michael Wekesa <wekesa360@outlook.com>
2022-01-20 10:15:59 +0300

    Fix: fix bug

--- 0x1E-search_algorithms/0-linear.c
+++ 0x1E-search_algorithms/0-linear.c
@@ -8,7 +8,7 @@
  * 
  * Return: first index where value is located, or -1 on  failure
  */
-int linear linear_search(int *array, size_t *size, int value)
+int linear_search(int *array, size_t size, int value)
 {
     size_t i;
     int tmp;
